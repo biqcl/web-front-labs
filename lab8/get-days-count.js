cdeb9e8deee9f596e7735a254a3fe1f3fdbc316f
Бызова Мария <m_byzovaa@mail.ru>
2024-05-28 16:09:24 +0700

    Количество дней с даты рождения

---
+++ lab8/get-days-count.js
@@ -0,0 +1,14 @@
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+
+    document.getElementById("days").innerText = `Количество дней с даты рождения: ${daysCount}`;
+}
+
+function clearInput() {
+    document.querySelector('input[type=date]').value = '';
+    document.getElementById("days").innerText = '';
+}
