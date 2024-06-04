f3721d243cee5bad136d0900482a896cba1f0343
Бызова Мария <m_byzovaa@mail.ru>
2024-06-04 21:04:41 +0700

    edited

--- lab8/time.js
+++ lab8/time.js
@@ -5,12 +5,11 @@ function showTime() {
 
     const seconds = today.getSeconds();
     const secondsDisplay = document.getElementById('seconds');
-    const secondHand = document.getElementById('clockwise');
-
+    const clockWise = document.getElementById('clockwise');
    
     secondsDisplay.innerHTML = seconds.toString().padStart(2, '0');
    
     const secondsDegree = ((seconds / 60) * 360) + 90; 
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    clockWise.style.transform = `rotate(${secondsDegree}deg)`;
 }
 setInterval(showTime, 1000);
